08bda3660cb68b5a696ff206541c585d0097c6e3
omkarpatne6 <omkarpatne6@gmail.com>
2023-11-25 18:27:30 +0530

    fixed reloading issue and added loader functionality

--- pages/collections/[collectionId].js
+++ pages/collections/[collectionId].js
@@ -6,27 +6,25 @@ import { CgWebsite } from "react-icons/cg";
 import { AiOutlineInstagram, AiOutlineTwitter } from "react-icons/ai";
 import { HiDotsVertical } from "react-icons/hi";
 import NFTCard from "../../components/NFTCard";
-import {
-  useContract,
-  useDirectListings,
-} from "@thirdweb-dev/react";
+import { useContract, useDirectListings } from "@thirdweb-dev/react";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const style = {
   bannerImageContainer: `h-[20vh] w-fit overflow-hidden flex justify-center items-center`,
   bannerImage: `w-full object-cover`,
   infoContainer: `w-full px-4`,
   midRow: `w-full flex justify-center text-white`,
-  endRow: `w-full flex justify-end text-white`,
+  endRow: `w-full flex justify-center text-white mb-5 mt-2`,
   profileImg: `w-40 h-40 object-cover rounded-full border-2 border-[#202225] mt-[-4rem]`,
-  socialIconsContainer: `flex text-3xl mb-[-2rem]`,
+  socialIconsContainer: `flex text-3xl`,
   socialIconsWrapper: `w-44`,
   socialIconsContent: `flex container justify-between text-[1.4rem] border-2 rounded-lg px-2`,
   socialIcon: `my-2`,
   divider: `border-r-2`,
   title: `text-5xl font-bold mb-4`,
   createdBy: `text-lg mb-4`,
-  statsContainer: `w-[44vw] flex justify-between py-4 border border-[#151b22] rounded-xl mb-4`,
-  collectionStat: `w-1/4`,
+  statsContainer: `sm:w-[100%] md:w-[80%] lg:w-[60%] flex gap-5 justify-evenly p-4 border border-[#151b22] rounded-xl mb-4`,
+  collectionStat: `w-fit`,
   statValue: `text-3xl font-bold w-full flex items-center justify-center`,
   ethLogo: `h-6 mr-2`,
   statName: `text-lg w-full text-center mt-1`,
@@ -35,7 +33,6 @@ const style = {
 
 const Collection = () => {
   const router = useRouter();
-  // const { provider } = useWeb3();
   const { collectionId } = router.query;
   const [collection, setCollection] = useState({});
 
@@ -50,22 +47,22 @@ const Collection = () => {
       title, floorPrice,
       "allOwners": owners[]->,
       description
-    }`
+    }`;
 
-    const collectionData = await sanityClient.fetch(query)
+    const collectionData = await sanityClient.fetch(query);
 
-    console.log(collectionData, '🔥')
+    console.log(collectionData, "🔥");
 
     // the query returns 1 object inside of an array
-    await setCollection(collectionData[0])
-  }
+    setCollection(collectionData[0]);
+  };
 
   useEffect(() => {
-    fetchCollectionData()
-  }, [collectionId])
+    fetchCollectionData();
+  }, [collectionId]);
 
   // console.log(router.query);
-  console.log('susssssssssssssssssssssssssss', router.query.collectionId);
+  console.log(router.query.collectionId);
 
   const { contract } = useContract(
     "0x7603Cdeb4101bDD5b6f4C152c1EffF61bcB5bfD3",
@@ -104,6 +101,10 @@ const Collection = () => {
             alt="profile image"
           />
         </div>
+
+        <div className={style.midRow}>
+          <div className={style.title}>{collection?.title}</div>
+        </div>
         <div className={style.endRow}>
           <div className={style.socialIconsContainer}>
             <div className={style.socialIconsWrapper}>
@@ -128,9 +129,6 @@ const Collection = () => {
           </div>
         </div>
         <div className={style.midRow}>
-          <div className={style.title}>{collection?.title}</div>
-        </div>
-        <div className={style.midRow}>
           <div className={style.createdBy}>
             Created by{" "}
             <span className="text-[#2081e2]">{collection?.creator}</span>
@@ -140,13 +138,13 @@ const Collection = () => {
           <div className={style.statsContainer}>
             <div className={style.collectionStat}>
               <div className={style.statValue}>{nfts?.length}</div>
-              <div className={style.statName}>items</div>
+              <div className={style.statName}>Items</div>
             </div>
             <div className={style.collectionStat}>
               <div className={style.statValue}>
                 {collection?.allOwners ? collection.allOwners.length : ""}
               </div>
-              <div className={style.statName}>owners</div>
+              <div className={style.statName}>Owners</div>
             </div>
             <div className={style.collectionStat}>
               <div className={style.statValue}>
@@ -157,7 +155,7 @@ const Collection = () => {
                 />
                 {collection?.floorPrice}
               </div>
-              <div className={style.statName}>floor price</div>
+              <div className={style.statName}>Floor price</div>
             </div>
             <div className={style.collectionStat}>
               <div className={style.statValue}>
@@ -168,7 +166,7 @@ const Collection = () => {
                 />
                 {collection?.volumeTraded}.5K
               </div>
-              <div className={style.statName}>volume traded</div>
+              <div className={style.statName}>Volume traded</div>
             </div>
           </div>
         </div>
@@ -190,7 +188,9 @@ const Collection = () => {
           ))}
         </div>
       ) : (
-        <div>Loading....</div>
+        <div className="flex justify-center items-center my-11">
+          <CircularProgress />
+        </div>
       )}
     </div>
   );
